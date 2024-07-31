f2f10df525c8f6eb6b4ee4d10f613d2c0d101282
Dylan Zucker <dyzucks@gmail.com>
2024-07-31 14:43:39 -0400

    ELSER

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ es = Elasticsearch(
             request_timeout=3600
         )
 
-def text_to_chunks(texts: str, chunk_length: int = 150, chunk_overlap: int = 25) -> list:
+def text_to_chunks(texts: str, chunk_length: int = 500, chunk_overlap: int = 100) -> list:
         """
         Splits the text into equally distributed chunks with 25-word overlap.
         Args:
@@ -46,34 +46,60 @@ def text_to_chunks(texts: str, chunk_length: int = 150, chunk_overlap: int = 25)
             chunk_number += 1
 
         return chunks
+import requests
+import json
+import io
+def _hash_content(content: str) -> str:
+    return _hash_content_bytes(content.encode())
 
+import hashlib
+
+def _hash_content_bytes(content_bytes: bytes) -> str:
+    return hashlib.sha256(content_bytes).hexdigest()
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk 
+import time
 def upload_pdf_to_elasticsearch(file, file_name, index_name="colgate_palmolive"):
-    embedding_model_name="sentence-transformers/all-MiniLM-L6-v2"
-    model = SentenceTransformer(embedding_model_name)
+    # embedding_model_name="sentence-transformers/all-MiniLM-L6-v2"
+    # model = SentenceTransformer(embedding_model_name)
     pdf_reader = PdfReader(file)
+    index = "search-colgate_elser"
+    PIPELINE_NAME = "ml-inference-search-colgate_elser-_elser_model_2_linux-x86_64"#"ml-inference-colgate_palmolive-_elser_model_2_linux-x86_64"
     text = " ".join(page.extract_text() if page.extract_text() else "" for page in pdf_reader.pages)
     chunks = text_to_chunks(texts=text, chunk_length=200, chunk_overlap=25)
+    action_chunks = []
     for i, chunk in enumerate(chunks):
-        embedding = model.encode(chunk['text'], convert_to_tensor=True).tolist()  # Convert tensor to list directly
+        document = {
+            "_index": index,
+            "index": index,
+            "_source": {
+                "text": chunk,
+                "text_field": chunk,
+            },
+            "pipeline": PIPELINE_NAME
+        }
+        action_chunks.append(document)
+
+    for i, chunk in enumerate(chunks):
+        url = "https://345a75c1-eb06-4713-abc0-3ed4aff540a2.c5kn1n9d0g7polghe820.databases.appdomain.cloud:30087/colgate_palmolive/_doc?pipeline=colgate_palmolive"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "ApiKey VXk3SUNKRUJ2RU1felZmUFRGNDc6LTVUVWhtZmNTUlNGVDltMkdhaXJVQQ=="
+        }
+
         doc = {
             "pdf_name": file_name.replace(' ', '_').lower(),
             "text": chunk['text'],
+            "text_field": chunk['text'],
             "chunk_number": i + 1,
-            "embedding": embedding
+             "_extract_binary_content": True,
+            "_reduce_whitespace": True,
+            "_run_ml_inference": True,
         }
-        res = es.index(index=index_name, body=doc)
-        
-        print(f"Added chunk {i} of {file_name}")
-    # # Create a document with the extracted text and some metadata
-    # doc = {
-    #     "file_name": file.name,
-    #     "content": text,
-    #     "page_count": pdf_reader.getNumPages()
-    # }
-    
-    # # Upload the document to Elasticsearch
-    # res = es.index(index=index_name, body=doc)
-    return res
+
+        res = es.index(index=index, body=doc, pipeline=PIPELINE_NAME)
+    return res 
 
 st.title("PDF to Elasticsearch Uploader")
 uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type="pdf")
@@ -85,5 +111,4 @@ if uploaded_files:
         # Upload the file to Elasticsearch
         result = upload_pdf_to_elasticsearch(file_bytes, uploaded_file.name)
         st.write(f"Uploaded {uploaded_file.name} to Elasticsearch. Response: {result}")
-
 
